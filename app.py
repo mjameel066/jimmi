57c74ba6a3c3bd2a9f8a235a44883cbfd365b65a
prade7 <pradeep112.pk7@gmail.com>
2024-07-19 15:07:20 +0530

    resolved the circular import issue and deprecated modules issue

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
-from flask import Flask, g, escape, session, redirect, render_template, request, jsonify, Response
+from flask import Flask, g, session, redirect, render_template, request, jsonify, Response
 from Misc.functions import *
+import markupsafe
+import os
+import logging
+from logging.handlers import RotatingFileHandler
 
 app = Flask(__name__)
-app.secret_key = '#$ab9&^BB00_.'
+
+# Use environment variable for secret key or configure it securely
+app.secret_key = os.environ.get('SECRET_KEY', '#$ab9&^BB00_.')
 
 # Setting DAO Class
 from Models.DAO import DAO
@@ -10,9 +16,18 @@ from Models.DAO import DAO
 DAO = DAO(app)
 
 # Registering blueprints
-from routes.user import user_view
-from routes.book import book_view
-from routes.admin import admin_view
+from routes.user import create_user_view
+from routes.book import create_book_view
+from routes.admin import create_admin_view
+
+# Create and register the user, book, and admin blueprints
+user_view = create_user_view(DAO)
+book_view = create_book_view(DAO)
+admin_view = create_admin_view(DAO)
+
+app.register_blueprint(user_view)
+app.register_blueprint(book_view)
+app.register_blueprint(admin_view)
 
 # Registering custom functions to be used within templates
 app.jinja_env.globals.update(
@@ -20,6 +35,6 @@ app.jinja_env.globals.update(
     str=str,
 )
 
-app.register_blueprint(user_view)
-app.register_blueprint(book_view)
-app.register_blueprint(admin_view)
+
+if __name__ == '__main__':
+    app.run()
